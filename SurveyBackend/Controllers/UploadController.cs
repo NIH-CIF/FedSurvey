0cfdd2cce053f14f0babfb593339ae9c34d288f9
Sammidysam <sammidysam@gmail.com>
2021-06-25 13:33:56 -0400

    Set up framework of uploading files later

---
+++ SurveyBackend/Controllers/UploadController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Survey.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Survey.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : ControllerBase
+    {
+        private readonly CoreDbContext _context;
+
+        public UploadController(CoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Upload(List<IFormFile> files)
+        {
+            System.Diagnostics.Debug.WriteLine(files.Count);
+
+            return Ok();
+        }
+    }
+}
