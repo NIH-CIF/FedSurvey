534dfc3786a647cae64cba62457392285d734bcc
Sammidysam <sammidysam@gmail.com>
2021-06-25 10:58:08 -0400

    Set up for seed migration again

---
+++ SurveyBackend/Migrations/20210625145258_SeedDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Survey.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Process will be to destroy all data from tables that were created in this migration.
+        }
+    }
+}
