f6fc3ca6158fca9ab5b77dd041fac025a2a2b150
Sammidysam <sammidysam@gmail.com>
2021-06-30 17:43:30 -0400

    QuestionList component

--- FedSurvey/ClientApp/src/components/Home.js
+++ FedSurvey/ClientApp/src/components/Home.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import { Input, Label } from 'reactstrap';
+import { QuestionList } from './QuestionList';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     constructor(props) {
         super(props);
-        this.state = { executions: [] };
+        this.state = { executions: [], currentExecutionId: 0, loading: true };
     }
 
     componentDidMount() {
         this.populateExecutionData();
     }
 
-    f(thing) { console.log(thing) };
-
     render() {
-        return (
+        return !this.state.loading && (
             <div>
                 <Label for="executionSelect">Year</Label>
-                <Input type="select" name="executionSelect" id="executionSelect" onChange={e => this.f(e.target.value)}>
+                <Input type="select" name="executionSelect" id="executionSelect" onChange={e => this.setState({ currentExecutionId: e.target.value })} value={this.state.currentExecutionId}>
                     {this.state.executions.map(e => (
                         <option value={e.id} key={e.id}>{e.key}</option>
                     ))}
                 </Input>
+
+                <hr />
+
+                <QuestionList executionId={this.state.currentExecutionId} />
             </div>
         );
     }
@@ -31,6 +34,6 @@ export class Home extends Component {
     async populateExecutionData() {
         const response = await fetch('api/executions');
         const data = await response.json();
-        this.setState({ executions: data });
+        this.setState({ executions: data, currentExecutionId: data[0].id, loading: false });
     }
 }
---
+++ FedSurvey/ClientApp/src/components/QuestionList.js
@@ -0,0 +1,47 @@
+﻿import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+export class QuestionList extends Component {
+    static displayName = QuestionList.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { questionExecutions: [] };
+    }
+
+    componentDidMount() {
+        this.populateQuestionExecutionData();
+    }
+
+    componentDidUpdate(prevProps) {
+        this.populateQuestionExecutionData(prevProps);
+    }
+
+    render() {
+        return (
+            <div>
+                <ListGroup>
+                    {this.state.questionExecutions.map(qe => (
+                        <ListGroup horizontal key={qe.id} style={{paddingBottom: '0.8rem'}}>
+                            <ListGroupItem>
+                                {qe.position}
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                {qe.body}
+                            </ListGroupItem>
+                        </ListGroup>
+                    ))}
+                </ListGroup>
+            </div>
+        );
+    }
+
+    async populateQuestionExecutionData(prevProps) {
+        if (!this.props.executionId || (prevProps && prevProps.executionId === this.props.executionId))
+            return;
+
+        const response = await fetch('api/question-executions?' + new URLSearchParams({ 'execution-ids': [this.props.executionId] }));
+        const data = await response.json();
+        this.setState({ questionExecutions: data });
+    }
+}
