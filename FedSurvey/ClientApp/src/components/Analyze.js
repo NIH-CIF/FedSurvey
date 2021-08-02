70c42405f2d00581d06bdce9926ee93c75757733
Sammidysam <sammidysam@gmail.com>
2021-08-02 15:15:04 -0400

    UI adjustments from Day 1 feedback

--- FedSurvey/ClientApp/src/components/Analyze.js
+++ FedSurvey/ClientApp/src/components/Analyze.js
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-import { Input, Label } from 'reactstrap';
+import { Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 import { ResultsDataTable } from './ResultsDataTable';
@@ -11,16 +11,14 @@ export class Analyze extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            groupingVariable: 'dataGroupName',
-            sortingVariable: 'executionTime',
-            filters: {
-                'question-ids': [1],
-                'possible-response-names': ['Positive']
-            },
+            groupingVariable: null,
+            sortingVariable: null,
+            filters: {},
             questionExecutions: [],
             dataGroups: [],
             executions: [],
-            possibleResponses: []
+            possibleResponses: [],
+            setupComplete: false
         };
     }
 
@@ -28,8 +26,62 @@ export class Analyze extends Component {
         this.populateDropdownData();
     }
 
+    instructionalRow() {
+        const downAcrossSelected = this.state.groupingVariable !== null && this.state.sortingVariable !== null;
+        const filtersSelected = this.getNonEmptyFiltersCount() > 1;
+
+        return !this.state.setupComplete && (
+            <span>
+                {(!downAcrossSelected || !filtersSelected) && (
+                    <b style={{ marginRight: 4 }}>Setup:</b>
+                )}
+
+                {downAcrossSelected && !filtersSelected && (
+                    `Now select what values you will lock the other variables into for this table.
+                    You will be looking at all or some instances of your down variable and across variable, but only one instance of these variables.
+                    After selecting this, the last two select boxes will open, where you can select multiple instances to view part of the larger table.
+                    You will also be free to change any variable across the six boxes.`
+                )}
+                {!downAcrossSelected && !filtersSelected && (
+                    `Select what variable will go down your table and across your table.
+                    These variables will generally be what you are comparing.
+                    Note that Year can only be an across variable and Question can only be a down variable.`
+                )}
+            </span>
+        );
+    }
+
+    dropdownRow(dropdownVariables) {
+        return (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                {dropdownVariables.map(dv => {
+                    const isMulti = this.state.groupingVariable === dv.tableName || this.state.sortingVariable === dv.tableName;
+                    const isDisabled = isMulti ? this.getNonEmptyFiltersCount() < 2 : (this.state.groupingVariable === null || this.state.sortingVariable === null);
+
+                    return (
+                        <div key={'div' + dv.tableName} style={{ flex: 1 }}>
+                            <Label for={dv.tableName}>
+                                {dv.displayName}{isMulti && 's'}
+                            </Label>
+                            <Select
+                                name={dv.tableName}
+                                isMulti={isMulti}
+                                isDisabled={isDisabled}
+                                id={dv.tableName}
+                                onChange={val => this.updateFilters(dv, val)}
+                                value={this.getFiltersForSelect(dv)}
+                                options={this.state[dv.listName].map(item => ({ label: item[dv.displayValue], value: item[dv.storeValue] }))}
+                                placeholder={isMulti ? 'Select one or more...' : 'Select...'}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+
     render() {
-        const totalVariables = ['dataGroupName', 'executionTime', 'possibleResponseName', 'questionText'];
+        const totalVariables = ['dataGroupName', 'questionText', 'possibleResponseName', 'executionTime'];
         const rowCols = [this.state.groupingVariable, this.state.sortingVariable];
         const notRowCols = totalVariables.filter(v => !rowCols.includes(v));
 
@@ -76,111 +128,99 @@ export class Analyze extends Component {
                 return {};
             }
         });
-        const dropdownFilterVariables = totalExpandedVariables.filter(tv => notRowCols.includes(tv.tableName));
-        const dropdownSelectVariables = totalExpandedVariables.filter(tv => rowCols.includes(tv.tableName));
+
+        const downOptions = notRowCols
+            .concat(this.state.groupingVariable || [])
+            .filter(v => v !== 'executionTime')
+            .map(v => (
+                { label: totalExpandedVariables.find(dv => dv.tableName === v).displayName, value: v }
+            ));
+        const acrossOptions = notRowCols
+            .concat(this.state.sortingVariable || [])
+            .filter(v => v !== 'questionText')
+            .map(v => (
+                { label: totalExpandedVariables.find(dv => dv.tableName === v).displayName, value: v }
+            ));
 
         return (
             <div>
-                <div>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Link to='/'>Home</Link>
+
+                    <a href="#" onClick={e => { e.preventDefault(); this.reset() }}>Reset</a>
                 </div>
 
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <div style={{ flex: 1 }}>
-                        <Label for="acrossVariable">
-                            Across Variable
-                        </Label>
-                        <Input
-                            type="select"
-                            name="acrossVariable"
-                            id="acrossVariable"
-                            onChange={e => this.updateTableVariable(
-                                'sortingVariable',
-                                totalExpandedVariables.find(dv => dv.tableName === this.state.sortingVariable),
-                                totalExpandedVariables.find(dv => dv.tableName === e.target.value),
-                                e.target.value
-                            )}
-                            value={this.state.sortingVariable}
-                        >
-                            {notRowCols
-                                .concat(this.state.sortingVariable)
-                                .filter(v => v !== 'questionText')
-                                .map(v => (
-                                <option value={v} key={v}>{totalExpandedVariables.find(dv => dv.tableName === v).displayName}</option>
-                            ))}
-                        </Input>
-                    </div>
+                {this.instructionalRow()}
 
+                <div style={{ display: 'flex', alignItems: 'center' }}>
                     <div style={{ flex: 1 }}>
                         <Label for="downVariable">
                             Down Variable
                         </Label>
-                        <Input
-                            type="select"
+                        <Select
                             name="downVariable"
                             id="downVariable"
-                            onChange={e => this.updateTableVariable(
+                            onChange={val => this.updateTableVariable(
                                 'groupingVariable',
                                 totalExpandedVariables.find(dv => dv.tableName === this.state.groupingVariable),
-                                totalExpandedVariables.find(dv => dv.tableName === e.target.value),
-                                e.target.value
+                                totalExpandedVariables.find(dv => dv.tableName === val.value),
+                                val.value
                             )}
-                            value={this.state.groupingVariable}
-                        >
-                            {notRowCols
-                                .concat(this.state.groupingVariable)
-                                .filter(v => v !== 'executionTime')
-                                .map(v => (
-                                <option value={v} key={v}>{totalExpandedVariables.find(dv => dv.tableName === v).displayName}</option>
-                            ))}
-                        </Input>
+                            value={downOptions.find(dop => dop.value === this.state.groupingVariable)}
+                            options={downOptions}
+                        />
+                    </div>
+
+                    <div style={{ flex: 1 }}>
+                        <Label for="acrossVariable">
+                            Across Variable
+                        </Label>
+                        <Select
+                            name="acrossVariable"
+                            id="acrossVariable"
+                            onChange={val => this.updateTableVariable(
+                                'sortingVariable',
+                                totalExpandedVariables.find(dv => dv.tableName === this.state.sortingVariable),
+                                totalExpandedVariables.find(dv => dv.tableName === val.value),
+                                val.value
+                            )}
+                            value={acrossOptions.find(ao => ao.value === this.state.sortingVariable)}
+                            options={acrossOptions}
+                        />
                     </div>
                 </div>
 
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    {dropdownFilterVariables.map(dv => (
-                        <div key={'div' + dv.tableName} style={{ flex: 1 }}>
-                            <Label for={dv.tableName}>
-                                {dv.displayName}
-                            </Label>
-                            <Select
-                                name={dv.tableName}
-                                id={dv.tableName}
-                                onChange={val => this.updateFilters(dv, val)}
-                                value={this.getFiltersForSelect(dv)}
-                                options={this.state[dv.listName].map(item => ({ label: item[dv.displayValue], value: item[dv.storeValue] }))}
-                            />
-                        </div>
-                    ))}
-                </div>
+                {this.dropdownRow(totalExpandedVariables.slice(0, 2))}
+                {this.dropdownRow(totalExpandedVariables.slice(2, 4))}
 
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    {dropdownSelectVariables.map(dv => (
-                        <div key={'div' + dv.tableName} style={{ flex: 1 }}>
-                            <Label for={dv.tableName}>
-                                {dv.displayName}
-                            </Label>
-                            <Select
-                                name={dv.tableName}
-                                isMulti
-                                id={dv.tableName}
-                                onChange={val => this.updateFilters(dv, val)}
-                                value={this.getFiltersForSelect(dv)}
-                                options={this.state[dv.listName].map(item => ({label: item[dv.displayValue], value: item[dv.storeValue]}))}
-                            />
-                        </div>
-                    ))}
-                </div>
-
-                <div>
-                    <ResultsDataTable
-                        filters={this.state.filters}
-                        groupingVariable={this.state.groupingVariable}
-                        sortingVariable={this.state.sortingVariable}
-                    />
-                </div>
+                {this.state.groupingVariable && this.state.sortingVariable && this.getNonEmptyFiltersCount() > 1 && (
+                    <div>
+                        <ResultsDataTable
+                            filters={this.state.filters}
+                            groupingVariable={this.state.groupingVariable}
+                            sortingVariable={this.state.sortingVariable}
+                        />
+                    </div>
+                )}
             </div>
         );
+    }
+
+    reset() {
+        this.setState({
+            setupComplete: false,
+            sortingVariable: null,
+            groupingVariable: null,
+            filters: {}
+        });
+    }
+
+    getNonEmptyFiltersCount() {
+        // needs to move
+        Object.filter = (obj, predicate) =>
+            Object.fromEntries(Object.entries(obj).filter(predicate));
+
+        return Object.keys(Object.filter(this.state.filters, ([key, value]) => value.length > 0)).length;
     }
 
     getFilters(dropdownFilterVariable) {
@@ -194,16 +234,18 @@ export class Analyze extends Component {
     updateFilters(dropdownFilterVariable, value) {
         const setValue = Array.isArray(value) ? value.map(v => v.value) : [value.value];
 
-        this.setState(prevState => ({ filters: { ...prevState.filters, [dropdownFilterVariable.filterKey]: setValue } }));
+        this.setState(prevState => ({
+            filters: { ...prevState.filters, [dropdownFilterVariable.filterKey]: setValue },
+            setupComplete: prevState.setupComplete || this.getNonEmptyFiltersCount() > 0
+        }));
     }
 
     updateTableVariable(variableName, currentDropdownVariable, dropdownFilterVariable, value) {
         this.setState(prevState => ({
-            filters: {
+            filters: dropdownFilterVariable !== null ? {
                 ...prevState.filters,
-                [dropdownFilterVariable.filterKey]: [],
-                [currentDropdownVariable.filterKey]: [this.state[currentDropdownVariable.listName][0][currentDropdownVariable.storeValue]]
-            },
+                [dropdownFilterVariable.filterKey]: []
+            } : prevState.filters,
             [variableName]: value,
         }));
     }
