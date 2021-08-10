8a3da2512124ad83f6e2123f1cf082f4e5316fb9
Sammidysam <sammidysam@gmail.com>
2021-08-10 14:25:37 -0400

    Add a script for switching the view config to be more OD-focused

---
+++ FedSurvey/od-view-config.sql
@@ -0,0 +1,93 @@
+﻿-- View 1
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 5;
+
+UPDATE ViewConfigs
+SET VariableValue = 'NATIONAL INSTITUTES OF HEALTH (NIH)'
+WHERE Id = 6;
+
+DELETE FROM ViewConfigs
+WHERE Id = 7;
+
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 9;
+
+-- View 2
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 15;
+
+UPDATE ViewConfigs
+SET VariableValue = 'NATIONAL INSTITUTES OF HEALTH (NIH)'
+WHERE Id = 16;
+
+DELETE FROM ViewConfigs
+WHERE Id = 17;
+
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 19;
+
+-- View 3
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 25;
+
+UPDATE ViewConfigs
+SET VariableValue = 'NATIONAL INSTITUTES OF HEALTH (NIH)'
+WHERE Id = 26;
+
+DELETE FROM ViewConfigs
+WHERE Id = 27;
+
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 29;
+
+-- View 4
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 34;
+
+-- View 5
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 42;
+
+-- View 6
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 50;
+
+-- View 7
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 59;
+
+-- View 8
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 68;
+
+UPDATE ViewConfigs
+SET VariableValue = 'NATIONAL INSTITUTES OF HEALTH (NIH)'
+WHERE Id = 69;
+
+UPDATE Views
+SET Name = 'Top Positive Response Strengths Relative to NIH'
+WHERE Id = 8;
+
+-- View 9
+UPDATE ViewConfigs
+SET VariableValue = 'OFFICE OF THE DIRECTOR (OD)'
+WHERE Id = 78;
+
+UPDATE ViewConfigs
+SET VariableValue = 'NATIONAL INSTITUTES OF HEALTH (NIH)'
+WHERE Id = 79;
+
+UPDATE Views
+SET Name = 'Top Positive Response Weaknesses Relative to NIH'
+WHERE Id = 9;
