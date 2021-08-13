ccbc4c2a7651f8548c87fe7ffb6cb9be0a8f3a6d
Sammidysam <sammidysam@gmail.com>
2021-08-13 10:38:18 -0400

    Documentation API route and start of Documentation List

---
+++ FedSurvey/ClientApp/src/components/DocumentationList.js
@@ -0,0 +1,42 @@
+﻿import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import api from '../api';
+
+export class DocumentationList extends Component {
+    static displayName = DocumentationList.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            markdownFiles: [],
+            selectedFile: null
+        };
+    }
+
+    componentDidMount() {
+        this.populateMarkdownFiles();
+    }
+
+    render() {
+        return this.state.markdownFiles.length > 0 && (
+            <div>
+                <h2>Documentation</h2>
+
+                {this.state.selectedFile !== null ? (
+                    <Documentation file={this.state.selectedFile} />
+                ) : (
+                    <ul>
+                        {this.state.markdownFiles.map(mf => (
+                            <li key={mf}>{mf.replace('.md', '').split('-').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        );
+    }
+
+    async populateMarkdownFiles() {
+        api.fetch('api/documentation')
+            .then(async (resp) => this.setState({ markdownFiles: await resp.json() }));
+    }
+}
---
+++ FedSurvey/Controllers/DocumentationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+
+namespace FedSurvey.Controllers
+{
+    [ApiController]
+    public class DocumentationController : ControllerBase
+    {
+        [Route("api/[controller]")]
+        [HttpGet]
+        public IActionResult List()
+        {
+            return new JsonResult(
+                Directory.GetFiles(@"./ClientApp/public/docs")
+                .Select(file => file.Replace("./ClientApp/public/docs\\", ""))
+            );
+        }
+    }
+}
